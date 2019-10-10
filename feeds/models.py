798fadf5bf9ac412efb6c00e78fee633c18eff55
changho <ckdghdus@naver.com>
2019-10-10 18:01:38 +0900

    insta

---
+++ feeds/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from imagekit.models import ProcessedImageField
+from imagekit.processors import Thumbnail, ResizeToFill
+# Create your models here.
+
+
+class Feed(models.Model):
+    content = models.CharField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = ProcessedImageField(
+        processors=[ResizeToFill(300, 300)],
+        format='JPEG',
+        options={'quality': 90},
+        upload_to='feeds',
+
+    )
